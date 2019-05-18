fbc009d007876288f41c4b97c8efc32e245117d1
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-05-18 23:09:16 +0100

    echo, wborder, wattron, getch returns racket chars

--- api.rkt
+++ api.rkt
@@ -8,11 +8,11 @@
 
 (define stdscr (make-parameter #f))
 
-(define (attron . attrs)
-  (ffi:attron (foldl (lambda (attr res)
-                       (bitwise-ior attr res))
-                     0
-                     attrs)))
+(define (attron #:win [win (stdscr)] . attrs)
+  (ffi:wattron win (foldl (lambda (attr res)
+                            (bitwise-ior attr res))
+                          0
+                          attrs)))
 
 (define (addstr str #:win [win (stdscr)]
                     #:y [y (ffi:getcury win)]
@@ -52,58 +52,22 @@
     (ffi:mvwaddch win y x ch)))
 
 (define (getch [win (stdscr)])
-  (ffi:wgetch win))
+  (integer->char (ffi:wgetch win)))
 
-; (define addch
-;   (case-lambda
-;     [(win ch)
-;      (let ([ch (char->integer ch)])
-;        (ffi:waddch win ch))]
-;     [(win ch attr)
-;      (let ([ch (char->integer ch)])
-;        (ffi:waddch win (bitwise-ior ch attr)))]
-;     [(win y x ch)
-;      (let ([ch (char->integer ch)])
-;        (ffi:mvwaddch win y x (bitwise-ior ch A_NORMAL)))]
-;     [(win y x ch attr)
-;      (let ([ch (char->integer ch)])
-;        (ffi:mvwaddch win y x (bitwise-ior ch attr)))]))
-
-;  (define rkt_addstr
-;    (case-lambda
-;      [(str) (addstr str)]
-;      [(str attr)
-;       (attron attr)
-;       (addstr str)
-;       (attroff attr)]
-;      [(y x str) (mvaddstr y x str)]
-;      [(y x str attr)
-;       (attron attr)
-;       (mvaddstr y x str)
-;       (attroff attr)]))
-;
-;  (define rkt_waddstr
-;    (case-lambda
-;      [(win str) (waddstr str)]
-;      [(win str attr)
-;       (attron attr)
-;       (waddstr win str)
-;       (attroff attr)]
-;      [(win y x str) (mvwaddstr win y x str)]
-;      [(win y x str attr)
-;       (wattron win attr)
-;       (mvwaddstr win y x str)
-;       (wattroff win attr)]))
-;
-
-(define (border [ch0 0] [ch1 0] [ch2 0] [ch3 0]
-                [ch4 0] [ch5 0] [ch6 0] [ch7 0])
-  (ffi:border ch0 ch1 ch2 ch3 ch4 ch5 ch6 ch7))
+(define (border #:win [win (stdscr)]
+                 [ch0 0] [ch1 0] [ch2 0] [ch3 0]
+                 [ch4 0] [ch5 0] [ch6 0] [ch7 0])
+  (ffi:wborder win ch0 ch1 ch2 ch3 ch4 ch5 ch6 ch7))
 
 (define (getmaxyx [win (stdscr)])
   (values (ffi:getmaxy win) (ffi:getmaxx win)))
 (define (get-curyx win)
   (values (ffi:getcury win) (ffi:getcurx win)))
+
+(define (echo opt)
+  (if opt
+    (ffi:echo)
+    (ffi:noecho)))
 
 (define attr_get ffi:attr_get)
 (define initscr ffi:initscr)
@@ -114,10 +78,13 @@
   (arithmetic-shift n 8))
 
 (define (with-ncurses func
-                      #:color [color #t])
+                      #:color? [color? #t]
+                      #:echo?  [echo? #f])
   (stdscr (initscr))
-  (when (and (ffi:has_colors) color)
+  (when (and (ffi:has_colors) color?)
     (ffi:start_color))
+  (unless echo?
+    (ffi:noecho))
   (define init? #t)
   (define (cleanup!)
     (when init?
