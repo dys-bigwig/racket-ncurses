30fe7d06c57f61934b45b2b41806d685616be6a4
dys-bigwig <45558889+dys-bigwig@users.noreply.github.com>
2019-06-25 22:23:27 +0100

    Update test.rkt

--- test.rkt
+++ test.rkt
@@ -2,16 +2,24 @@
 (require ncurses)
 
 (define (test)
+  (noecho!)
+  (curs-set 0)
+  (border)
   (define-values (screen-height screen-width)
     (getmaxyx))
   (init-pair! 1 COLOR_WHITE COLOR_RED)
   (let ([ch (getch)])
   (addstr (format "YOU PRESSED ~a!" ch)
-            (color-pair 1) A_BOLD
+          
+            (color-pair 1) A_BOLD A_UNDERLINE
             #:y (quotient screen-height 2)
             #:x (quotient (- screen-width
                              (string-length "YOU PRESSED ~a!"))
                           2)))
-  (getch))
+  (define foowin (newwin 10 10 4 4))
+  (addstr "WIZARDS" #:win foowin)
+  (refresh)
+  (getch #:win foowin)
+  (delwin foowin))
 
 (with-ncurses test)
